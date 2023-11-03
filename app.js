d12d09d1f8aa5d013eeb2aed95070829f950d8f5
Andy Vanosdale <andy@waviisoft.com>
2023-11-03 15:45:06 -0700

    Vercel KV for lookup

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express')
-const app = express()
+const vercelkv = require('@vercel/kv')
+
 const PORT = 3000
+const app = express()
+const kv = vercelkv.kv
 
-const redirect = 'https://www.waviisoft.com/'
+app.get('/*', async (req, res, next) => {
+  const { hostname } = req
+  const redirect = await kv.get(hostname)
+  if (!redirect) {
+    next(new Error(`Host: ${hostname} - not found`))
+    return
+  }
 
-app.get('/*', (req, res) => {
-  console.log(`Host: ${req.hostname}`)
+  console.log(`Host: ${hostname}`)
   res.status(302).header('Location', redirect).send(`<a href="${redirect}">Found</a>`)
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.message)
+  res.status(404).send('not found')
 })
 
 app.listen(PORT, () => {
